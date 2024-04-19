678f7138415eeec4f531ad8d8926e94ff1973779
Yellapu Rama <y.rama2024@gmail.com>
2024-04-19 22:39:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoadMoreData from './component/LoadMoreData'
+
+function App() {
+  return (
+  <LoadMoreData/>
+  )
+}
+
+export default App
---
+++ src/component/LoadMoreData.jsx
@@ -0,0 +1,66 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function LoadMoreData() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data ! Please wait.</div>;
+  }
+
+  return (
+    <div className="load-more-container bg-slate-400 ">
+        <h1 className="flex justify-center text-2xl p-4">Shoppers stop</h1>
+      <div className="product-container grid grid-cols-4 gap-2 ">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product border flex flex-col gap-2 p-3" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container flex flex-col justify-center">
+        <button className=" border bg-blue-400 p-2 rounded-xl " disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disableButton ? <p>You have reached to 100 products</p> : null}
+      </div>
+    </div>
+  );
+}
